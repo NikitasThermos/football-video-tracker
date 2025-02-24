e2cdde8217130324fba61b4c7b2860c12f15c323
NikitasThermos <nikiras.t@gmail.com>
2025-02-24 11:58:26 +0200

    drawing annotations on video

--- src/football_analysis/main.py
+++ src/football_analysis/main.py
@@ -1,9 +1,15 @@
-from utils import read_video
+from drawing import draw_annotations
+from video_utils import read_video, save_video
+
+from tracker import get_object_tracks
 
 
 def main():
     video_frames = read_video('videos/input_videos/08fd33_4.mp4')
-    return video_frames
+    tracks = get_object_tracks(video_frames, read_file=True, file_path='tracks/tracks.npy')
+    output_frames = draw_annotations(video_frames, tracks)
+
+    save_video(output_frames, 'videos/output_videos/annotated_video.mp4')
 
 
 if __name__ == '__main__':
